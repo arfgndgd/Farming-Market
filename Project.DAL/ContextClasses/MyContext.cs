48de77b6bf1cc852b45be7be6d6dc3c6e3d05747
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-16 16:22:05 +0300

    Context işlemleri tamamlandı

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,51 @@
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public class MyContext:DbContext
+    {
+
+        public MyContext() : base("MyConnection")
+        {
+            //Database.SetInitializer(new MyInit()); 
+            //TODO: SetInitializer eklemeli miyim?
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AppUserMap());
+            modelBuilder.Configurations.Add(new UserProfileMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new EmployeeMap());
+            modelBuilder.Configurations.Add(new DepartmentMap());
+            modelBuilder.Configurations.Add(new OrderDetailMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new ShipperMap());
+            modelBuilder.Configurations.Add(new SupplierMap());
+            modelBuilder.Configurations.Add(new StorageMap());
+            modelBuilder.Configurations.Add(new StorageCategoryMap());
+            modelBuilder.Configurations.Add(new StorageOrderDetailMap());
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<StorageCategory> StorageCategories { get; set; }
+        public DbSet<StorageOrderDetail> StorageOrderDetails { get; set; }
+    }
+}
---
+++ Project.DAL/StrategyPatterns/MyInit.cs
@@ -0,0 +1,18 @@
+﻿using Project.DAL.ContextClasses;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.StrategyPatterns
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            //TODO: Admin işlemi nasıl yapılacak çünkü Employee kısmından yönetim istiyoruz
+        }
+    }
+}
