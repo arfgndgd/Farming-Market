6e1721b590abf9bf0e72ba425974585613b7eeba
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-17 17:48:53 +0300

    RegisterController tamamlandı

---
+++ Project.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,83 @@
+﻿using Project.BLL.DesignPatterns.GenericRepositories.ConcRep;
+using Project.COMMON.Tools;
+using Project.ENTITIES.Models;
+using Project.WebUI.Models.VMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        AppUserRepository apRep;
+        UserProfileRepository usRep;
+        public RegisterController()
+        {
+            apRep = new AppUserRepository();
+            usRep = new UserProfileRepository();
+        }
+
+        // GET: Register
+        public ActionResult RegisterNow()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterNow(AppUserVM apvm)
+        {
+            AppUser appUser = apvm.AppUser;
+            UserProfile profile = apvm.UserProfile;
+
+            //TODO: şifreleme sınııfın ekle 
+
+            //Kayıt işlemi
+            if (apRep.Any(x=>x.UserName == appUser.UserName))
+            {
+                ViewBag.ZatenVar = "Kullanıcı ismi alınmış";
+                return View();
+            }
+            else if (apRep.Any(x=>x.Email == appUser.Email))
+            {
+                ViewBag.ZatenVar = "Email kayıtlı";
+                return View();
+            }
+
+            //Başarılı kayıt sonrası mail gönderme işlemi
+            string hesapKayit = "Tebrikler, hesabınız oluşturulmuştur. Hesabınızı aktive etmek için https://localhost:44317/Register/Activation/" + appUser.ActivationCode + "linkine tıklayabilirsiniz.";
+
+            MailSender.Send(appUser.Email, body: hesapKayit, subject: "Hesap Aktivasyon!");
+            apRep.Add(appUser);
+
+            if (!string.IsNullOrEmpty(profile.FirstName) || !string.IsNullOrEmpty(profile.LastName) || !string.IsNullOrEmpty(profile.Address))
+            {
+                profile.ID = appUser.ID;
+                usRep.Add(profile);
+            }
+            return View("RegisterOk");
+        }
+
+        public ActionResult RegisterOk()
+        {
+            return View();
+        }
+
+        public ActionResult Activation(Guid id)
+        {
+            AppUser hesapAktif = apRep.FirstOrDefault(x => x.ActivationCode == id);
+            if (hesapAktif != null)
+            {
+                hesapAktif.Active = true;
+                apRep.Update(hesapAktif);
+
+                TempData["HesapAktifmi"] = "Hesap aktivasyonu tamamlandı";
+                return RedirectToAction("Login", "Home");
+            }
+            TempData["HesapAktifmi"] = "Aktif edilecek hesap bulunamadı";
+            return RedirectToAction("Login","Home");
+        }
+    }
+}
---
+++ Project.WebUI/Models/VMClasses/AppUserVM.cs
@@ -0,0 +1,14 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.WebUI.Models.VMClasses
+{
+    public class AppUserVM
+    {
+        public AppUser AppUser { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
