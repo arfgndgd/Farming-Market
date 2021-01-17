e9b5dbcfebde18c4ff8120d8018d83e98161d5c2
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-17 03:24:02 +0300

    ImageUploader sınıfı tamamlandı

--- Project.COMMON/Tools/ImageUploader.cs
+++ Project.COMMON/Tools/ImageUploader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,44 @@ using System.Web;
 
 namespace Project.COMMON.Tools
 {
-    public class ImageUploader
+    public static class ImageUploader
     {
-        
+        public static string UploadImage(string serverPath, HttpPostedFileBase file)
+        {
+            if (file !=null)
+            {
+                Guid uniqueName = Guid.NewGuid();
+
+                serverPath = serverPath.Replace("~", string.Empty);
+
+                string[] fileArray = file.FileName.Split('.');
+
+                string extension = fileArray[fileArray.Length - 1].ToLower();
+
+                string fileName = $"{uniqueName}.{extension}";
+
+                if (extension == "jpg" || extension == "gif" || extension == "png" || extension == "jpeg")
+                {
+                    if (File.Exists(HttpContext.Current.Server.MapPath(serverPath + fileName)))
+                    {
+                        return "1"; //dosyanı ismini sorgular aynı ismi kullanmaması için
+                    }
+                    else
+                    {
+                        string filePath = HttpContext.Current.Server.MapPath(serverPath + fileName);
+                        file.SaveAs(filePath);
+                        return serverPath + fileName;
+                    }
+                }
+                else
+                {
+                    return "2"; //uygun formatta değil
+                }
+            }
+            else
+            {
+                return "3"; //dosya boş
+            }
+        }
     }
 }
