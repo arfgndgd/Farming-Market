e165a5c646915301e743b05659789e9b16664e2a
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-26 00:03:41 +0300

    StorageVM classı eklendi

---
+++ Project.WebUI/Models/VMClasses/StorageVM.cs
@@ -0,0 +1,18 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.WebUI.Models.VMClasses
+{
+    public class StorageVM
+    {
+        public List<Storage> Storages { get; set; }
+        public Storage Storage { get; set; }
+        public List<StorageCategory> StorageCategories { get; set; }
+        public List<Supplier> Suppliers { get; set; }
+
+        
+    }
+}
