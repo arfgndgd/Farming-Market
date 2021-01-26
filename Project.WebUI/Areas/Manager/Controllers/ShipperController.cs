4049ce70349e4efa618bda1bbe7062b41328972c
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-26 22:36:23 +0300

    ShipperController tamamlandı

--- Project.WebUI/Areas/Manager/Controllers/ShipperController.cs
+++ Project.WebUI/Areas/Manager/Controllers/ShipperController.cs
@@ -1,4 +1,6 @@
 ﻿using Project.BLL.DesignPatterns.GenericRepositories.ConcRep;
+using Project.ENTITIES.Models;
+using Project.WebUI.Models.VMClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +19,60 @@ namespace Project.WebUI.Areas.Manager.Controllers
         // GET: Manager/Default
         public ActionResult ShipperByID(int id)
         {
-
+            ShipperVM svm = new ShipperVM
+            {
+                Shipper = sRep.Find(id)
+            };
             return View();
+        }
+        
+        public ActionResult ShipperList()
+        {
+            ShipperVM svm = new ShipperVM
+            {
+                Shippers = sRep.GetAll()
+            };
+            return View();
+        }
+
+        public ActionResult AddShipper()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddShipper(Shipper shipper)
+        {
+            sRep.Add(shipper);
+            return RedirectToAction("ShipperList");
+        }
+
+        public ActionResult UpdateShipper(int id)
+        {
+            ShipperVM svm = new ShipperVM
+            {
+                Shipper = sRep.Find(id)
+            };
+            return View(svm);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateShipper(Shipper shipper)
+        {
+            sRep.Update(shipper);
+            return RedirectToAction("ShipperList");
+        }
+
+        public ActionResult DeleteShipper(int id)
+        {
+            sRep.Delete(sRep.Find(id));
+            return RedirectToAction("ShipperList");
+        }
+
+        public ActionResult DestroyShipper(int id)
+        {
+            sRep.Destroy(sRep.Find(id));
+            return RedirectToAction("ShipperList");
         }
     }
 }
