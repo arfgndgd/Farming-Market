5f8acdac26704225c36edfbb6a12624a891b9bc9
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-29 22:44:03 +0300

    Cart classında Sepet işlemleri yapıldı

--- Project.WebUI/Models/ShoppingTools/Cart.cs
+++ Project.WebUI/Models/ShoppingTools/Cart.cs
@@ -7,7 +7,52 @@ namespace Project.WebUI.Models.ShoppingTools
 {
     public class Cart
     {
-        
+        Dictionary<int, CartItem> _sepetim;
+
+        public Cart()
+        {
+            _sepetim = new Dictionary<int, CartItem>();
+        }
+
+        //Sepet CartItem sınıfını da içinde tutar
+        public List<CartItem> Sepetim
+        {
+            get
+            {
+                return _sepetim.Values.ToList();
+            }
+        }
+
+        //Sepete ekleme CartItem içindeki Amount propu sayesinde arttırma yapar
+        public void SepeteEkle(CartItem item)
+        {
+            if (_sepetim.ContainsKey(item.ID))
+            {
+                _sepetim[item.ID].Amount += 1;
+                return;
+            }
+            _sepetim.Add(item.ID, item);
+        }
+
+        //Amount sayesinde yine çıkarma yapılabilir
+        public void SepettenSil(int id)
+        {
+            if (_sepetim[id].Amount > 1)
+            {
+                _sepetim[id].Amount--;
+                return;
+            }
+            _sepetim.Remove(id);
+        }
+
+        //Sepetin Toplamı (CartItem classında Price*Amount çarpımından gelen fiyat)
+        public decimal? TotalPrice
+        {
+            get
+            {
+                return _sepetim.Sum(x => x.Value.SubTotal);
+            }
+        }
 
 
     }
