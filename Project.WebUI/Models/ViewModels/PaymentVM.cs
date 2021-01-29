70e35a704ef96da1b7ea878f91413dcccf53d46c
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-01-29 21:55:56 +0300

    Sanal Post için model oluşturuldu

---
+++ Project.WebUI/Models/ViewModels/PaymentVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.WebUI.Models.ViewModels
+{
+    public class PaymentVM
+    {
+        //Sanal Post Entegrasyonu
+
+        public int ID { get; set; }
+        public string CardUserName { get; set; }
+        public string SecurityNumber { get; set; }
+        public string CardNumber { get; set; }
+        public int CardExpiryMonth { get; set; }
+        public int CardExpiryYear { get; set; }
+        public decimal ShoppingPrice { get; set; }
+
+
+
+    }
+}
